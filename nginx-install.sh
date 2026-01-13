a848357e1ad1ebf826587feae2c7c084a8027ceb
Tredecate <thomasdezwart@gmail.com>
2026-01-13 10:42:33 -0800

    fix scripts, comments, etc. and add proper output path

--- nginx-install.sh
+++ nginx-install.sh
@@ -1,33 +1,16 @@
 # nginx-install.sh
-# Script 1
+#!/bin/bash
 
-#!/usr/bin/env bash
+ssh -i "${SSH_KEY_PATH}" "${NAME}@${IP_ADDR}" << EOF
+# Update package lists
+sudo apt update -y
 
-source ./config.env
+# Install Nginx
+sudo apt install nginx -y
 
-echo "Connect to server ${NAME}@${IP_ADDR} to install Nginx"
+# Start the Nginx service
+sudo systemctl start nginx
 
-ssh -i "${SSH_KEY_PATH}" "${NAME}@${IP_ADDR}" << 'EOF'
-
-    # Update package lists
-    echo "Updating package lists..."
-    sudo apt update -y
-
-    # Install Nginx
-    echo "Installing Nginx..."
-    sudo apt install nginx -y
-
-    # Start the Nginx service
-    echo "Starting Nginx..."
-    sudo systemctl start nginx
-
-    # Enable Nginx to start on boot
-    echo "Enabling Nginx..."
-    sudo systemctl enable nginx
-
-    # Verify status
-    sudo systemctl status nginx --no-pager | head -n 5
-
+# Enable Nginx to start on boot
+sudo systemctl enable nginx
 EOF
-
-echo "Nginx installation complete."
